bb17dbfa862f9bc4dad19e88ccf7d99be5985994
echo webid <saya@echo.web.id>
2019-04-05 22:46:52 +0700

    Add path assets

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,7 +14,7 @@ var sourcemaps = require('gulp-sourcemaps')
 
 // Include HTML
 gulp.task('include-html', () => {
-    return gulp.src('app/**/*.html')
+    return gulp.src(['app/**/*.html', 'app/**/**/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: 'app'
@@ -23,13 +23,19 @@ gulp.task('include-html', () => {
         .pipe(gulp.dest('.tmp'));
 });
 
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('app/assets/scripts/**/*')
+    .pipe(gulp.dest('.tmp/assets/scripts'))
+});
+
 // Styles
 gulp.task('styles', function() {
     return gulp.src([
-        'app/styles/**/*.scss',
-        'app/styles/**/**/*.scss',
-        'app/styles/**/*.css',
-        'app/styles/**/**/*.css',
+        'app/assets/styles/**/*.scss',
+        'app/assets/styles/**/**/*.scss',
+        'app/assets/styles/**/*.css',
+        'app/assets/styles/**/**/*.css',
     ])
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
@@ -43,25 +49,31 @@ gulp.task('styles', function() {
     }))
     .pipe(size({title: 'styles'}))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('.tmp/styles'));
+    .pipe(gulp.dest('.tmp/assets/styles'));
 });
 
 // Fonts
 gulp.task('fonts', function() {
     return gulp.src([
-        'app/styles/webfonts/**/*'
+        'app/assets/styles/webfonts/**/*'
     ], {
         dot: true
     })
     .pipe(size({title: 'fonts'}))
-    .pipe(gulp.dest('.tmp/styles/webfonts'));
+    .pipe(gulp.dest('.tmp/assets/styles/webfonts'));
 });
 
 // Optimize images
 gulp.task('images', function() {
-  return gulp.src('app/images/**/*')
-    .pipe(gulp.dest('.tmp/images'))
+  return gulp.src('app/assets/images/**/*')
+    .pipe(gulp.dest('.tmp/assets/images'))
     .pipe(size({title: 'images'}))
+});
+
+// Vendor
+gulp.task('vendor', function() {
+  return gulp.src('app/assets/vendor/**/*')
+    .pipe(gulp.dest('.tmp/assets/vendor'))
 });
 
 // Browser Sync Reload
@@ -81,11 +93,13 @@ gulp.task('browsersync', function() {
         port: 3000
     });
 
-    gulp.watch(['app/**/*.html'], gulp.series('include-html', 'reload'));
-    gulp.watch(['app/styles/webfonts/**/*'], gulp.series('fonts', 'reload'));
-    gulp.watch(['app/styles/**/*.{scss,css}'], gulp.series('styles', 'reload'));
-    gulp.watch(['app/images/**/*'], gulp.series('images', 'reload'));
+    gulp.watch(['app/**/*.html', 'app/**/**/*.html'], gulp.series('include-html', 'reload'));
+    gulp.watch(['app/assets/styles/webfonts/**/*'], gulp.series('fonts', 'reload'));
+    gulp.watch(['app/assets/styles/**/*.{scss,css}'], gulp.series('styles', 'reload'));
+    gulp.watch(['app/assets/images/**/*'], gulp.series('images', 'reload'));
+    gulp.watch(['app/assets/scripts/**/*'], gulp.series('scripts', 'reload'));
+    gulp.watch(['app/assets/vendor/**/*'], gulp.series('vendor', 'reload'));
 });
 
 // Serve Default
-gulp.task('serve', gulp.series('include-html', 'fonts', 'styles', 'images', 'browsersync'));
+gulp.task('serve', gulp.series('include-html', 'fonts', 'styles', 'scripts', 'images', 'vendor', 'browsersync'));
